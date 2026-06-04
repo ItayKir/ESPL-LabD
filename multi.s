d1292ad95a43c1aaefb973e187d5806d239a19f5
Itay Kigner <itaykirgner@gmail.com>
2026-06-04 14:25:54 +0000

    Part 2.A finished

--- multi.s
+++ multi.s
@@ -3,8 +3,13 @@ section .rodata
     format_newline: db 10, 0       
 
 section .data
+    ; First struct (size 5)
     x_struct: db 5
     x_num: db 0xaa, 0x01, 0x02, 0x44, 0x4f
+
+    ; Second struct (size 6)
+    y_struct: db 6
+    y_num: db 0xaa, 0x01, 0x02, 0x03, 0x44, 0x4f
 
 section .bss
     ; Reserve 500 bytes for our input buffer
@@ -14,6 +19,7 @@ section .text
     global main
     global print_multi
     global getmulti
+    global get_max_min
     extern printf
     extern fgets
     extern stdin
@@ -21,7 +27,7 @@ section .text
 
 ; ---------------------------------------------------------
 ; void print_multi(struct multi *p)
-; (Same as Part 1.A, included here for completeness)
+; Prints a multi-precision integer in hexadecimal.
 ; ---------------------------------------------------------
 print_multi:
     push ebp
@@ -74,17 +80,16 @@ getmulti:
     push edi
 
     ; 1. Read from stdin into input_buffer + 1
-    ; fgets(input_buffer + 1, 490, stdin)
     mov eax, [stdin]
-    push eax                      ; arg 3: FILE *stream
-    push 490                      ; arg 2: int size
-    push input_buffer + 1         ; arg 1: char *str
+    push eax                      
+    push 490                      
+    push input_buffer + 1         
     call fgets
-    add esp, 12                   ; Clean up 3 args
+    add esp, 12                   
 
     ; 2. Calculate the length of the inputted string 
     mov esi, input_buffer + 1
-    xor ecx, ecx                  ; ecx will act as our length counter
+    xor ecx, ecx                  
 .find_len:
     xor eax, eax
     mov al, byte [esi + ecx]
@@ -97,36 +102,38 @@ getmulti:
 
 .found_end:
     ; 3. Handle Odd vs Even length
-    mov ebx, input_buffer + 1     ; Default start pointer
+    mov ebx, input_buffer + 1     
     mov eax, ecx
-    and eax, 1                    ; Bitwise AND with 1 (checks if odd)
-    jz .is_even                   ; If zero, it's even
+    and eax, 1                    
+    jz .is_even                   
 
 .is_odd:
-    mov byte [input_buffer], '0'  ; Prepend a '0' to the buffer
-    mov ebx, input_buffer         ; Update start pointer to include the '0'
-    inc ecx                       ; Increase length by 1 (it is now even)
+    mov byte [input_buffer], '0'  
+    mov ebx, input_buffer         
+    inc ecx                       
 
 .is_even:
     ; 4. Allocate memory for struct multi
     mov eax, ecx
-    shr eax, 1                    ; eax = ecx / 2 (number of data bytes)
-    push eax                      ; Save the data size for later (to be popped into edx)
-    inc eax                       ; Add 1 for the struct's size byte
+    shr eax, 1                    
+    push eax                      
+    inc eax                       
     
-    ; --- THE FIX: PRESERVE ECX ---
-    push ecx                      ; malloc clobbers ecx, so we must save it!
-    push eax                      ; Push total allocation size
+    push ecx                      
+    push eax                      
     call malloc
-    add esp, 4                    ; Clean up malloc arg
-    pop ecx                       ; Restore our string length back into ecx!
-    ; -----------------------------
+    add esp, 4                    
+    pop ecx                       
 
-    pop edx                       ; edx = number of data bytes (struct size)
-    mov edi, eax                  ; edi = pointer to newly allocated struct
+    pop edx                       
+    mov edi, eax                  
 
     ; 5. Set the size byte in the struct
-    mov byte [edi], dl            ; p->size = edx
+    mov byte [edi], dl            
+
+    ; 6. Parse pairs of hex characters and write them backwards
+    dec edx                       
+    mov esi, ebx                  
 
 .parse_loop:
     cmp ecx, 0
@@ -136,26 +143,26 @@ getmulti:
     xor eax, eax
     mov al, byte [esi]
     call char_to_hex
-    shl al, 4                     ; Shift to the upper 4 bits
-    mov bl, al                    ; Temporarily store in bl
+    shl al, 4                     
+    mov bl, al                    
 
     ; Read second hex character (Lower nibble)
     xor eax, eax
     mov al, byte [esi + 1]
     call char_to_hex
-    or bl, al                     ; Combine with upper nibble
+    or bl, al                     
 
     ; Write the fully constructed byte to the struct
     mov byte [edi + 1 + edx], bl
 
     ; Update pointers and counters
-    add esi, 2                    ; Advance read pointer by 2 characters
-    dec edx                       ; Decrement write index
-    sub ecx, 2                    ; We processed 2 characters
+    add esi, 2                    
+    dec edx                       
+    sub ecx, 2                    
     jmp .parse_loop
 
 .done_parsing:
-    mov eax, edi                  ; Return the struct pointer in eax
+    mov eax, edi                  
 
     pop edi
     pop esi
@@ -184,18 +191,57 @@ char_to_hex:
     ret
 
 ; ---------------------------------------------------------
+; Part 2.A: get_max_min
+; Input:  eax = pointer to struct multi 1
+;         ebx = pointer to struct multi 2
+; Output: eax = pointer to struct with the larger size
+;         ebx = pointer to struct with the smaller size
+; Note:   Does NOT use the standard CDECL calling convention.
+; ---------------------------------------------------------
+get_max_min:
+    ; Save registers we will use for comparison
+    push ecx
+    push edx
+
+    ; Read the size byte from the first struct (pointed to by eax)
+    xor ecx, ecx
+    mov cl, byte [eax]
+    
+    ; Read the size byte from the second struct (pointed to by ebx)
+    xor edx, edx
+    mov dl, byte [ebx]
+
+    ; Compare sizes
+    cmp cl, dl
+    jge .done_max_min  ; If eax's struct size >= ebx's struct size, do nothing
+
+    ; If we drop down to here, the struct in ebx is larger.
+    ; Swap the pointers so eax holds the maximum!
+    xchg eax, ebx
+
+.done_max_min:
+    ; Restore used registers
+    pop edx
+    pop ecx
+    ret
+
+; ---------------------------------------------------------
 ; main
-; Entry point to test getmulti and print_multi
+; Entry point to test the functions
 ; ---------------------------------------------------------
 main:
     push ebp
     mov ebp, esp
 
-    ; Call getmulti to read from stdin
-    call getmulti
+    ; Setup inputs for get_max_min
+    mov eax, x_struct       ; size 5
+    mov ebx, y_struct       ; size 6
+
+    ; Call get_max_min (Custom Calling Convention)
+    call get_max_min
     
-    ; getmulti returns the struct pointer in eax. 
-    ; Pass it directly to print_multi to verify the input!
+    ; After this call, eax will strictly point to y_struct 
+    ; because 6 > 5. Let's verify by printing eax!
     push eax
     call print_multi
     add esp, 4            
